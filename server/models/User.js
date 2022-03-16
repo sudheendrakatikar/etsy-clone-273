29dd9a8474f58c0e565948e302b5c73c909b1e3f
Sudheendra Katikar <sudhu.katikar@gmail.com>
2022-03-16 14:45:59 -0700

    Basic User model

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../config/database')
+
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+})
+
+module.exports = User
